53d8444f8866767d1f95bd80ed2d3c1c3cef8bc5
Marco Rojo <mrmarcondes@gmail.com>
2020-04-22 17:53:12 -0300

    create cart service

---
+++ functions/src/services/cart.ts
@@ -0,0 +1,40 @@
+import { TransactionTypeRepository } from '../repositories/transactionType'
+import { CartRepository } from '../repositories/cart'
+
+const transactionTypeRepository = new TransactionTypeRepository()
+const cartRepository = new CartRepository()
+
+export class Cart {
+  async update(userId: string, referencePeriod: string, transaction: any) {
+    const transactionType = await transactionTypeRepository.getById(userId, transaction.transactionTypeId)
+    let cart = await cartRepository.get(userId, referencePeriod)
+    const transactionValue = parseFloat(transaction.value.replace(/,/g, '.'))
+    if (cart) {
+      cart = await this.updateCart(transactionType, cart, transactionValue)
+      await cartRepository.save(userId, referencePeriod, cart)
+    }
+  }
+
+  private async updateCart(transactionType: any, cart: any, transactionValue: any) {
+    if (transactionType._type === 'incomes') {
+      cart.income += transactionValue
+      return cart
+    } else {
+      const outcome = cart.outcome += transactionValue
+      return this.updateTransactionGroups(cart, transactionType, transactionValue, outcome)
+    }
+  }
+  
+  private updateTransactionGroups(cart: any, transactionType: any, transactionValue: number, outcome: number): any {
+    const transactionGroups = ['essential', 'whises', 'savings']
+
+    for(const transactionGroup of transactionGroups) {
+      let transactionGroupValue = cart[transactionGroup].value
+      if (transactionType.group === transactionGroup) {
+        transactionGroupValue = cart[transactionGroup].value += transactionValue
+      }
+      cart[transactionGroup].percentage = (transactionGroupValue / outcome) * 100
+    }
+    return cart
+  }
+}
--- functions/src/services/transaction.ts
+++ functions/src/services/transaction.ts
@@ -1,10 +1,8 @@
 import { TransactionRepository } from '../repositories/transaction'
-import { TransactionTypeRepository } from '../repositories/transactionType'
-import { CartRepository } from '../repositories/cart'
+import { Cart } from './cart'
 
 const transactionRepository = new TransactionRepository()
-const transactionTypeRepository = new TransactionTypeRepository()
-const cartRepository = new CartRepository()
+const cart = new Cart()
 export class Transaction {
   async createEmpty(data: any, context: any): Promise<any> {
     const emptyTransaction = await transactionRepository.createEmpty(context.auth.uid, data.referencePeriod)
@@ -23,44 +21,12 @@ export class Transaction {
     }
     const userId = context.auth.uid
     await transactionRepository.save(userId, referencePeriod, transaction)
-
-    // update incomes/ outcomes
-    const transactionType = await transactionTypeRepository.getById(userId, transaction.transactionTypeId)
-    const cart = await cartRepository.get(userId, referencePeriod)
-    const transactionValue = parseFloat(transaction.value.replace(/,/g, '.'))
-    if (cart) {
-      if (transactionType) {
-        if (transactionType._type === 'incomes') {
-          cart.income += transactionValue
-        } else {
-          const outcome = cart.outcome += transactionValue
-          const transactionGroups = ['essential', 'whises', 'savings']
-
-          for(const transactionGroup of transactionGroups) {
-            let transactionGroupValue = cart[transactionGroup].value
-            if (transactionType.group === transactionGroup) {
-              transactionGroupValue = cart[transactionGroup].value += transactionValue
-            }
-            cart[transactionGroup].percentage = (transactionGroupValue / outcome) * 100
-          }
-        }
-        await cartRepository.save(userId, referencePeriod, cart)
-      }
-    }
+    await cart.update(userId, referencePeriod, transaction)
   }
 
   async remove(data: any, context: any): Promise<any> {
     const referencePeriod = data.referencePeriod
     const transaction = { ...data.transaction }
     await transactionRepository.remove(context.auth.uid, referencePeriod, transaction)
-  }
-
-  async updateCart(userId: any, referencePeriod: any, transaction: any) {
-    // const transactionType = transactionTypeRepository.getById(userId, transaction.transactionTypeId)
-    const cart = await cartRepository.get(userId, referencePeriod)
-    if (cart) {
-      cart.income += transaction.value
-      await cartRepository.save(userId, referencePeriod, cart)
-    }
   }
 }
