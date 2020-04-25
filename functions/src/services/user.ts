b636bc5283c7cf97cf6214964f0df38c6eb8030d
Marco Rojo <mrmarcondes@gmail.com>
2020-04-25 18:16:59 -0300

    add transaction types for new users

--- functions/src/services/user.ts
+++ functions/src/services/user.ts
@@ -1,12 +1,17 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import { TransactionTypeRepository } from '../repositories/transactionType'
+import { AccountRepository } from '../repositories/account'
 
+const accountRepository = new AccountRepository()
+const transactionTypeRepository = new TransactionTypeRepository()
 export class User {
   async create(request: any, response: any): Promise<any> {
     try {
       const data = request
-      console.log(data)
       const user = await admin.auth().createUser({email: data.email, password: data.password, displayName: data.displayName})
+      await accountRepository.create(user.uid)
+      await transactionTypeRepository.copyAllGlobalToUser(user.uid)
       return { ...user }
     } catch (error) {
       throw new functions.https.HttpsError('internal', error);
